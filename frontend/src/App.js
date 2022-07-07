77141f098be40c118974471729fb57811c352acc
berrkantt <balkanz17@itu.edu.tr>
2022-07-07 23:23:38 +0300

    bismillah

---
+++ frontend/src/App.js
@@ -0,0 +1,45 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const crn = e.target.crn.value;
+    const token = e.target.token.value;
+    console.log("İstenen CRN'ler: " + crn, "\n", "Token: " + token)
+  }
+
+
+  return (
+
+
+    <div>
+      <header>ITU</header>
+      <h1>Lavuklar Software Sunar...</h1>
+      <h4>Since 1773</h4>
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="crn" placeholder="crn" /><br></br><br></br>
+        <input type="text" name="token" placeholder="token" /><br></br><br></br>
+        <button>Kaydet</button>
+      </form>
+      <br></br><br></br>
+      <br></br><br></br>
+    
+      <iframe 
+      width="560" 
+      height="315" 
+      src="https://www.youtube.com/embed/0Kyl2WakR1w" 
+      title="YouTube video player" 
+      frameBorder="0" 
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+      allowFullScreen></iframe>
+
+
+      {" "}
+    </div>
+  );
+}
+
+export default App;
